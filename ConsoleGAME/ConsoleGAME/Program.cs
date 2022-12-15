6fea96669f588b091b83db8ae123ead7b7a93837
parkkwanghyun1 <110909549+parkkwanghyun1@users.noreply.github.com>
2022-12-15 21:44:40 +0900

    2022.12.15(프로퍼티, stringbuilder)

--- ConsoleGAME/ConsoleGAME/Program.cs
+++ ConsoleGAME/ConsoleGAME/Program.cs
@@ -1,53 +1,93 @@
 ﻿using System;
+using System.Globalization;
 using System.Runtime.InteropServices;
 using System.Security.Cryptography.X509Certificates;
-namespace OtherGame
-{
-    class Data
-    {
-        public int value = 100;
-    }
- 
-}
-
+using System.Text;
 
 namespace ConsoleGame
 {
-    // 이름 공간(name space)이란?
-    // 내부 식별자에 범위를 제공해주는 선언적인
-    // 영역이다
+    class Unit
+    {
+        private int data; // <- data 어디에서나 접근이 가능하게 된다. 
+
+
+        
+        public int Data
+        {
+            set
+            {     // data <- Data 프로퍼티에서 받은 값 
+                if (value > 18)
+                {
+                    Console.WriteLine("Error Value");
+                    return;
+                }
+                data = value;
+            }
+            get
+            {
+                return data;
+            }
+        }
+
+        // 자동 구현 프로퍼티
+        // 자동 구현 프로퍼티는 선언을 했을 때 컴파일러가 자동으로
+        // private 필드를 생성한다.
+
+        public int Health
+        {
+            set;
+            get;
+        } = 200;
+
+        // 프로퍼티는 GET 만 사용해서 선언할 수 있다.
+        public  int Attack
+        {
+            get;
+        }
+    }
+
+  
     internal class Program
     {
         static void Main(string[] args)
         {
-            #region 반복문
+            #region 프로퍼티
             /*
-            // foreach
-            int[] item = new int[5];
+            Unit factory = new Unit();
 
-            // for
-            for (int i = 0; i < 5; i++)
-            {
-                item[i] = i;
-            }
-        
-            // 배열 , 리스트
-            // 고객 정보를 그대로 읽어 올 때 사용하면 된다. 
-            foreach(int element in item)
-            { 
-                // foreach 문 내부에서는
-                // 값을 변경할 수 없습니다.
+            factory.Data = 100;
 
-                Console.WriteLine(element);
+            factory.Health = 100;
+
+            Console.WriteLine("Unit 의 Health 값 : " + factory.Health);
+            Console.WriteLine("Unit 의 data 값 : " + factory.Data);
             */
             #endregion
 
 
-            // 같은 이름의 변수를 동시에 선언했기 때문에
-            // 컴파일 에러가 발생한다
-            int value = 10;
-            Console.WriteLine("ConsoleGame의 value : " + value);
+            // StringBuilder
+            StringBuilder score = new StringBuilder("100");
+
+            Console.WriteLine("score 변수의 값 : " + score);
+
+            // Replace
+            // 첫번째 매개변수에 있는 문자열을 두번째 매개변수에 있는
+            // 문자열로 변경하는 매소드이다.
+            score.Replace("100", "200");
+
+            Console.WriteLine("score 변수의 값 : " + score);
+
+            // Append
            
+                score.Append("의 값을 가지고 있습니다");
+
+            Console.WriteLine("score 변수의 값 : " + score);
+
+            // 가변 -> 객체가 인스턴스된 후에 변할 수 있는 객체
+            // 불변 -> 객체가 인스턴스된 후에 변할 수 없는 객체
+
+            string name = "Park";
+            name = "LEE";
             
         }
     }
---
+++ ConsoleGAME/namespace/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using OtherGame;
+
+namespace OtherGame
+{
+    class Data
+    {
+        public int value = 100;
+    }
+
+}
+
+
+namespace ConsoleGame
+{
+    // 이름 공간(name space)이란?
+    // 내부 식별자에 범위를 제공해주는 선언적인
+    // 영역이다
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 반복문
+            /*
+            // foreach
+            int[] item = new int[5];
+
+            // for
+            for (int i = 0; i < 5; i++)
+            {
+                item[i] = i;
+            }
+        
+            // 배열 , 리스트
+            // 고객 정보를 그대로 읽어 올 때 사용하면 된다. 
+            foreach(int element in item)
+            { 
+                // foreach 문 내부에서는
+                // 값을 변경할 수 없습니다.
+
+                Console.WriteLine(element);
+            */
+            #endregion
+
+            #region 이름 공간(name space) 
+            /*
+            Data data = new Data();
+            data.value = 100;
+
+            Console.WriteLine("OtherGame의 vlaue : " + data);
+            // 같은 이름의 변수를 동시에 선언했기 때문에
+            // 컴파일 에러가 발생한다
+            int value = 10;
+            Console.WriteLine("ConsoleGame의 value : " + value);
+            */
+            #endregion
+
+        }
+    }
+}
